203f42a5d23895b2dd09f93930d8ce028f091680
sumeet21428 <sumeet21428@iiitd.ac.in>
2023-06-22 00:57:33 +0530

    ..

--- graphgen/settings.py
+++ graphgen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q#ey&x#!3_smbug$=z$9#z7+gjhc^=g428^jtc+*55n(z$j%d^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
-ALLOWED_HOSTS =['127.0.0.1']
+ALLOWED_HOSTS =['.vercel.app']
 
 
 
